9583163b795063e54692701dc7978449e5fa26ec
gducklow <gducklow@gmail.com>
2024-02-17 10:12:56 -0600

    added my own api key to js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const app = {
-    init: () => {
-      document
-        .getElementById("search-weather")
-        .addEventListener("click", app.fetchLocation);
-    },
-}  
+var searchInput = $("#search-city")
+var searchBtn = $("#search-btn")
+
+function getWeather(){
+ fetch("https://api.openweathermap.org/data/2.5/weather?q="+ searchInput.val() +"&units=imperial&&appid=92b9ce2f29ad4ef45834acfb54dccfd0")
+ .then(res => res.json())
+ .then(data =>{
+  console.log(data)
+
+  $("#city-name").text(data.name);
+
+  $("#current-temp").text("Temp: " + data.main.temp)
+
+ })
+}
+searchBtn.on("click", getWeather)
